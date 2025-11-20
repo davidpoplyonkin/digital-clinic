638486993f0012d3b902718d0b717b25427c4e2e
Davyd Poplyonkin <dp.8cbpm@gmail.com>
2025-11-20 18:25:32 +0200

    Feat: Add Patient detail view

--- apps/patients/urls.py
+++ apps/patients/urls.py
@@ -5,6 +5,8 @@ app_name = "patients"
 
 urlpatterns = [
     path("", views.patient_list_view, name="patient-list"),
-    path("<int:pk>/", views.patient_detail_view, name = "patient-detail"),
-    path("new/", views.patient_create_view, name="patient-create")
+    path("<int:pk>/", views.patient_detail_view, name="patient-detail"),
+    path("new/", views.patient_create_view, name="patient-create"),
+    path("<int:pk>/update/", views.patient_update_view, name="patient-update"),
+    path("<int:pk>/delete/", views.patient_delete_view, name="patient-delete")
 ]
--- apps/patients/views.py
+++ apps/patients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 
 from .models import Patient
@@ -26,8 +26,46 @@ def patient_list_view(request):
 
     return render(request, "patients/patient_list.html", context)
 
-def patient_detail_view(request):
-    pass
+def patient_detail_view(request, pk):
+
+    obj = get_object_or_404(Patient, pk = pk)
+
+    obj_fields = []
+    
+    for field in obj._meta.fields:
+        if field.name not in {"id", "full_name"}:
+            obj_fields.append({
+                "name": field.verbose_name.capitalize(),
+                "value": getattr(obj, field.name)
+            })      
+    
+    context = {
+        "app": "patients",
+        "obj": obj,
+        "obj_fields": obj_fields,
+        "additional_buttons": [
+            {
+                "url": "patients:patient-update",
+                "bootstrap_class": "btn-outline-secondary",
+                "bootstrap_icon": "bi-pencil",
+                "text": "Edit",
+            }, {
+                "url": "patients:patient-delete",
+                "bootstrap_class": "btn-outline-danger",
+                "bootstrap_icon": "bi-trash",
+                "text": "Delete",
+            }
+        ],
+        "url_list": "patients:patient-list"
+    }
+    
+    return render(request, "patients/patient_detail.html", context)
 
 def patient_create_view(request):
+    pass
+
+def patient_update_view(request):
+    pass
+
+def patient_delete_view(request):
     pass
