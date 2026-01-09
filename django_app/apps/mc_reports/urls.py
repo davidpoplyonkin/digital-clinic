f3540ad7548527ea0eb62f99dd16a6e603254d12
Davyd Poplyonkin <dp.8cbpm@gmail.com>
2026-01-09 19:03:38 +0200

    Feat: Add MC Reports create and update views

--- django_app/apps/mc_reports/urls.py
+++ django_app/apps/mc_reports/urls.py
@@ -7,7 +7,7 @@ app_name = "mc_reports"
 urlpatterns = [
     path("", views.mcr_list_view, name="mcr-list"),
     path("<int:pk>/", views.mcr_detail_view, name="mcr-detail"),
-    path("new/", views.mcr_upsert_view, name="mcr-create"),
-    path("<int:pk>/update/", views.mcr_upsert_view, name="mcr-update"),
+    path("new/", views.MCReportCreateView.as_view(), name="mcr-create"),
+    path("<int:pk>/update/", views.MCReportUpdateView.as_view(), name="mcr-update"),
     path("<int:pk>/delete/", views.mcr_delete_view, name="mcr-delete"),
 ]
