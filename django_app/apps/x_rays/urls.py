ea08b60c6cf914453c11fb85b88a3c1cc2003b56
Davyd Poplyonkin <dp.8cbpm@gmail.com>
2026-01-23 09:55:51 +0200

    Feat: Add X-rays create and update views

--- django_app/apps/x_rays/urls.py
+++ django_app/apps/x_rays/urls.py
@@ -7,5 +7,6 @@ app_name = "x_rays"
 urlpatterns = [
     path("", views.x_rays_list_view, name="x-rays-list"),
     path("<int:pk>/", views.x_rays_detail_view, name="x-rays-detail"),
-    path("new/", views.x_rays_create_view, name="x-rays-create"),
+    path("new/", views.XRaysCreateView.as_view(), name="x-rays-create"),
+    path("<int:pk>/update/", views.XRaysUpdateView.as_view(), name="x-rays-update"),
 ]
--- django_app/apps/x_rays/views.py
+++ django_app/apps/x_rays/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import XRaysExamination
+from .forms import XRaysForm
 
 @login_required
 def x_rays_list_view(request):
@@ -33,12 +36,31 @@ def x_rays_detail_view(request, pk):
         "obj": obj,
         "buttons": {
             "back": "x_rays:x-rays-list",
+            "edit": "x_rays:x-rays-update",
         }
     }
     
     return render(request, "x_rays/x_rays_detail.html", context)
 
 
-@login_required
-def x_rays_create_view(request):
-    pass
+class XRaysCreateView(LoginRequiredMixin, CreateView):
+    model = XRaysExamination
+    form_class = XRaysForm
+    template_name = "x_rays/x_rays_form.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["app"] = "x_rays"
+
+        return context
+
+class XRaysUpdateView(LoginRequiredMixin, UpdateView):
+    model = XRaysExamination
+    form_class = XRaysForm
+    template_name = "x_rays/x_rays_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["app"] = "x_rays"
+
+        return context
